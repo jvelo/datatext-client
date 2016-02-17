9ad023fcc4a5dc7dfa67205631641bdf519213b8
Jérôme Velociter <jerome@velociter.fr>
2016-02-17 16:21:17 +0100

    WIP assets and shortcodes

--- app/datatext/page/page.ts
+++ app/datatext/page/page.ts
@@ -20,20 +20,36 @@ import {Page as PageModel} from '../services/pages';
   ]
 })
 export class HTMLContent implements OnChanges {
+
   private content: string;
-  constructor(@Inject(ElementRef) private elementRef: ElementRef) {
+
+  constructor( @Inject(ElementRef) private elementRef: ElementRef) {
   }
-  ngOnChanges(changes: {[propertyName: string]: SimpleChange}) {
-    this.elementRef.nativeElement.innerHTML = this.content;
-    const tables = this.elementRef.nativeElement.querySelectorAll('table');
-    Array.prototype.forEach.call(tables, node => node.classList.add('table'));
+
+  ngOnChanges(changes: { [propertyName: string]: SimpleChange }) {
+    const fragment = document.createDocumentFragment(),
+          wrapper = document.createElement('div');
+
+    wrapper.innerHTML = this.content;
+
+    const scripts = wrapper.querySelectorAll('script');
+    fragment.appendChild(wrapper);
+
+    this.elementRef.nativeElement.innerHTML = wrapper.innerHTML;
+
+    [].forEach.call(scripts, script => {
+      const payload = script.innerHTML,
+            element = document.createElement('script');
+      element.text = payload;
+      document.body.appendChild(element);
+    });
   }
 }
 
 @Component({
   selector: 'page',
   templateUrl: './datatext/page/page.html',
-  directives: [ HTMLContent ]
+  directives: [HTMLContent]
 })
 export class Page implements OnInit {
 
@@ -45,13 +61,24 @@ export class Page implements OnInit {
     private router: Router,
     private routeParams: RouteParams,
     private pagesService: PagesService
-  ) {
+    ) {
+  }
+
+  fetchPage() {
+    const id = this.id || this.routeParams.get('id');
+    this.pagesService.getPage(id).subscribe(page => this.page = page,
+      err => console.error(err));
   }
 
   ngOnInit() {
-    let id = this.id || this.routeParams.get('id');
-    this.pagesService.getPage(id).subscribe(page => this.page = page,
-                                            err => console.error(err));
+    this.fetchPage();
+
+    this.pagesService.editPage.subscribe(page => {
+      if (page === undefined) {
+        // Stopped editing : reload page
+        this.fetchPage();
+      }
+    });
   }
 
   editPage() {
@@ -59,6 +86,6 @@ export class Page implements OnInit {
   }
 
   openRevisions() {
-    this.router.navigate( ['PageRevisions', { id: this.page.id }] );
+    this.router.navigate(['PageRevisions', { id: this.page.id }]);
   }
 }
